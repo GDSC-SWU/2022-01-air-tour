74492a3329cc9ea399cd5b4199c5bc7550e1557e
Queue-ri <qriositylog@gmail.com>
2022-11-11 22:05:37 +0900

    feat: Add ExplorePage (#8)

---
+++ src/pages/ExplorePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PackageGrid from '../components/explore/v1/PackageGrid';
+
+function ExplorePage() {
+    return (
+        <div style={{padding: '0 80px'}}>
+        <PackageGrid />
+        </div>
+    );
+}
+
+export default ExplorePage;
